83a0454b5b8526e399d4f6ed436c054cd19eff34
R-JAWAHA-R <r.jawahar2007@gmail.com>
2026-03-04 18:17:58 +0530

    changed the contents of the main/src file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let data = fs::read(&args.file)?;
+    println!("Loaded ARM Thumb binary: {} ({} bytes)", args.file, data.len());
+    println!("First 16 bytes: {:02X?}", &data[..data.len().min(16)]);
+    Ok(())
 }
