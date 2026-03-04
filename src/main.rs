1d827711e6c9b864173a66b7be63edfa6a39225a
R-JAWAHA-R <r.jawahar2007@gmail.com>
2026-03-04 18:31:39 +0530

    .....

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use clap::Parser;
 use std::fs;
 use anyhow::Result;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(help = "Path to ARM Thumb ELF file")]
+    file: String,
+}
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let data = fs::read(&args.file)?;
     println!("Loaded ARM Thumb binary: {} ({} bytes)", args.file, data.len());
-    println!("First 16 bytes: {:02X?}", &data[..data.len().min(16)]);
+    println!("First 16 bytes: {:02X?}", 
+             &data[..data.len().min(16)]);
     Ok(())
 }
